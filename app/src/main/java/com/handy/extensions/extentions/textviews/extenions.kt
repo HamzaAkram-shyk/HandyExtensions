e5b63dedde96cff8bd8a5eef7b66a8c9cc5e0c35
hamza-akram-sct <hamza.akram@appiskey.com>
2022-11-11 18:53:32 +0500

    add Read more extention function of textview with smooth animation

--- app/src/main/java/com/handy/extensions/extentions/textviews/extenions.kt
+++ app/src/main/java/com/handy/extensions/extentions/textviews/extenions.kt
@@ -1,13 +1,17 @@
 package com.handy.extensions.extentions.textviews
 
+import android.graphics.Color
 import android.os.Build
-import android.text.Layout
-import android.text.StaticLayout
-import android.text.TextPaint
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.Gravity
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.ColorRes
 import com.handy.extensions.extentions.fadInAnimation
 import com.handy.extensions.extentions.fadOutAnimation
+import com.handy.extensions.extentions.increaseViewSizeWithAnimation
 
 fun TextView.setTextAnimation(
     text: String,
@@ -42,4 +46,54 @@ fun TextView.getHeightBeforeRender(text: CharSequence = getText()): Int {
             lineSpacingMultiplier, lineSpacingExtra, true
         ))
     }.height
+}
+
+
+fun TextView.makeResizableTextView(
+    visibleCharacterCount: Int,
+    @ColorRes readMoreColor: Int,
+    readMoreVerbiage: String = "read more",
+    animationDuration: Long = 300L
+) {
+    val actualText = this.text
+    if (actualText.length <= visibleCharacterCount)
+        return
+    val span =
+        SpannableString(actualText.substring(0, visibleCharacterCount) + "... $readMoreVerbiage")
+    val context = this.context
+    val clickableSpan: ClickableSpan = object : ClickableSpan() {
+        override fun onClick(view: View) {
+            this@makeResizableTextView.text = actualText.toString()
+            this@makeResizableTextView.getHeightBeforeRender().also {
+                this@makeResizableTextView.increaseViewSizeWithAnimation(
+                    increaseValue = it,
+                    duration = animationDuration
+                )
+            }
+
+
+        }
+
+        override fun updateDrawState(ds: TextPaint) {
+            super.updateDrawState(ds)
+            ds.isUnderlineText = false
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    ds.color = context.resources.getColor(readMoreColor, context.theme)
+                } else {
+                    ds.color = context.resources.getColor(readMoreColor)
+                }
+            } catch (e: Exception) {
+                ds.color = Color.parseColor("#000000")
+
+            }
+
+        }
+    }
+
+    span.setSpan(clickableSpan, span.length - 10, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    this.text = span
+    this.movementMethod = LinkMovementMethod.getInstance()
+
+
 }
